9dfebd1247c1ea558e0a395387a053b1e6eda76e
graffe <graffe@graffe.co>
2024-03-15 10:55:05 -0700

    removing vite config stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-import reactRefresh from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import svgrPlugin from 'vite-plugin-svgr';
 import vercel from 'vite-plugin-vercel';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -10,25 +7,8 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  define: {
-    'process.env': {},
-    global: {
-      window: {
-        origin: 'http://localhost:3000',
-      },
-    },
-  },
   plugins: [
     vercel(),
-    nodePolyfills(),
     tsconfigPaths(),
-    reactRefresh({
-      include: '**/*.tsx',
-    }),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-      },
-    }),
   ],
 });
