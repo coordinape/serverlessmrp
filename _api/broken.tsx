32798ccdb7e48d42a3c1bc5b3cba5eace103e8d6
graffe <graffe@graffe.co>
2024-03-18 11:27:04 -0700

    tyring new fix

--- _api/broken.tsx
+++ _api/broken.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { ImageResponse } from '@vercel/og';
-import ReactDOM from 'react-dom/server';
+import { Readable } from 'node:stream';
+import type { ReadableStream } from 'node:stream/web';
 
 // @ts-ignore
 export default async function handler(req: VercelRequest, res: VercelResponse) {
@@ -13,9 +14,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const ir = new ImageResponse(s);
     console.log(ir);
 
-    // convert s to a string
-    const sString = ReactDOM.renderToString(s);
-    return res.status(200).send(sString);
+    Readable.fromWeb(ir.body as ReadableStream<any>).pipe(res);
   } catch (e: any) {
     console.error(`${e.message}`);
     return res.status(500).send(`Failed to generate the image: ${e.message}`);
