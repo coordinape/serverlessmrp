0306c09943cc1779e038f0b01ba461709bea2440
graffe <graffe@graffe.co>
2024-03-25 19:29:11 -0400

    f

--- _api/FrameImage2.tsx
+++ _api/FrameImage2.tsx
@@ -6,6 +6,11 @@ import {VercelRequest, VercelResponse} from '@vercel/node';
 import { ImageResponse } from '@vercel/og';
 
 export default async function handler(req: VercelRequest, res: VercelResponse){
-  const ir = new ImageResponse(<div>horse</div>);
+  const ir = new ImageResponse(<Moo/>);
   Readable.fromWeb(ir.body as ReadableStream<any>).pipe(res);
 };
+
+
+const Moo = () => {
+  return <div>horse</div>;
+}
